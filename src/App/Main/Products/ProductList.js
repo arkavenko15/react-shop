b5793cd907d134c34bcdfcc9cb2edfcef2737d1e
Liuda Arkavenko <arkavenko18@gmail.com>
2021-03-15 10:07:01 +0200

    added products array

--- src/App/Main/Products/ProductList.js
+++ src/App/Main/Products/ProductList.js
@@ -1,45 +1,27 @@
 import React from 'react'
 import ProductListItem from './ProductListItem'
+import products from './products'
 const ProductList = () =>
 {
     return(
         <>
             <h1 className="page-title">Product List</h1>
             <div className="row">
-                <div className="col-lg-6">
-                    <ProductListItem
-                    name="iPhone 8"
-                    description="This is IPhone 8"
-                    type="phone"
-                    capacity="256GB"
-                    price="1200"/>
-                </div>
-                <div className="col-lg-6">
-                <ProductListItem
-                    name="iPhone 7"
-                    description="This is IPhone 7"
-                    type="phone"
-                    capacity="128GB"
-                    price="1000"/>
-                </div>
-            </div>
-            <div className="row">
-                <div className="col-lg-6">
-                <ProductListItem
-                    name="iPhone X"
-                    description="This is IPhone X"
-                    type="phone"
-                    capacity="256GB"
-                    price="1250"/>
-                </div>
-                <div className="col-lg-6">
-                <ProductListItem
-                    name="iPhone XS"
-                    description="This is IPhone XS"
-                    type="phone"
-                    capacity="128GB"
-                    price="1230"/>
-                </div>
+            {
+                products.map(product =>(
+                
+                    <div className="col-lg-6">
+                        <ProductListItem
+                        name={product.name}
+                        description={product.description}
+                        type={product.type}
+                        capacity={product.capacity}
+                        price={product.price}
+                        />
+                    </div>
+                ))
+
+            }
             </div>
         </>
     )
---
+++ src/App/Main/Products/products.js
@@ -0,0 +1,32 @@
+const products =
+[
+    {
+        name:"iPhone 8",
+        description:"This is IPhone 8",
+        type:"phone",
+        capacity:"256GB",
+        price:"1300",
+    },
+    {
+        name:"iPhone 7",
+        description:"This is IPhone 7",
+        type:"phone",
+        capacity:"256GB",
+        price:"1400",
+    },
+    {
+        name:"iPhone X",
+        description:"This is IPhone X",
+        type:"phone",
+        capacity:"512GB",
+        price:"1600",
+    },
+    {
+        name:"iPhone XS",
+        description:"This is IPhone XS",
+        type:"phone",
+        capacity:"256GB",
+        price:"1200",
+    },
+]
+export default products
